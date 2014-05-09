8dd9225b79acd30ad5b41bee8878a732cadb95d8
Oscar David Arbeláez <odarbelaeze@gmail.com>
2014-05-09 18:31:04 -0500

    Little test and pyflake

--- pcsample/sample.py
+++ pcsample/sample.py
@@ -1,33 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import lxml.etree as et
-from subprocess import call
+#import lxml.etree as et
+#from subprocess import call
 
 # My own utilities
 import utils
 
+
 class Sample(object):
     """docstring for Sample"""
     def __init__(
-        self, a, rad, d, l,
+        self, rad, d, l, a=1.0,
         tmpprefix="sample", in_rand=False, out_rand=True
     ):
-        
+
         super(Sample, self).__init__()
-        self.a = a
         self.rad = rad
         self.d = d
         self.l = l
+        self.a = a
         self.tmpprefix = tmpprefix
         self.in_rand = in_rand
         self.out_rand = out_rand
-        
+
     def macro_lattice_parameter(self):
         """
-        Returns the lattice parameter for the triangular lattice given 
+        Returns the lattice parameter for the triangular lattice given
         the conditions.
         """
-        return self.a * (2.0 * self.rad + d)
+        return self.a * (2.0 * self.rad + self.d)
 
     def x_param(self):
         """
@@ -39,7 +40,7 @@ class Sample(object):
         """
         Yields the lattice parameter in the y direction of the lattice.
         """
-        return 2.0 * np.sin(np.degrees(120)) * self.macro_lattice_parameter()
+        return np.sin(np.degrees(120)) * self.macro_lattice_parameter()
 
     def x_max(self):
         """
@@ -57,27 +58,27 @@ class Sample(object):
         """
         Returns the sample size.
         """
-        return np.array([ self.x_max(), self.y_max(), ])
+        return np.array([self.x_max(), self.y_max(), ])
 
     def cell_size(self):
         """
         Returns the single cubic cell size.
         """
-        return np.array([ self.x_param(), self.y_param(), ])
+        return np.array([self.x_param(), self.y_param(), ])
 
     def super_cell_size(self):
         """
         Returns the dual in y cubic cell size.
         """
-        return np.array([ self.x_param(), 2.0 * self.y_param(), ])
+        return np.array([self.x_param(), 2.0 * self.y_param(), ])
 
     def regular_lattice(self):
         """
         Returns a regular lattice filling the whole sample.
         """
         # TODO: Cache this return
-        xtics = np.arange(0, self.x_max(), self.a)
-        ytics = np.arange(0, self.y_max(), a)
+        xtics = np.arange(0.0, self.x_max(), self.a)
+        ytics = np.arange(0.0, self.y_max(), self.a)
         return np.array([[x, y] for x in xtics for y in ytics])
 
     def random_lattice(self):
@@ -85,43 +86,62 @@ class Sample(object):
         Returns a random lattice filling the whole sample.
         """
         #TODO: This may be a function, but I'm gessing is only used once
-        n = int((l * (2 * rad + d)) ** 2)
+        n = int((self.l * (2 * self.rad + self.d)) ** 2)
         xrand = np.random.uniform(0, self.x_max(), n)
         yrand = np.random.uniform(0, self.x_max(), n)
-        
+
         # TODO: Cache this return
         return np.column_stack((xrand, yrand))
 
     def centers(self):
         return (
-            np.array([ 0.0, 0.0, ]) * self.cell_size(),
-            np.array([ 0.5, 1.0, ]) * self.cell_size(),
-            np.array([ 1.0, 0.0, ]) * self.cell_size(),
-            np.array([ 0.0, 2.0, ]) * self.cell_size(),
-            np.array([ 1.0, 2.0, ]) * self.cell_size(),
+            np.array([0.0, 0.0, ]) * self.cell_size(),
+            np.array([0.5, 1.0, ]) * self.cell_size(),
+            np.array([1.0, 0.0, ]) * self.cell_size(),
+            np.array([0.0, 2.0, ]) * self.cell_size(),
+            np.array([1.0, 2.0, ]) * self.cell_size(),
         )
 
-    def in_circles(self, positions):
+    def in_circles(self):
         """
         Return the portion of the sample inside the circles.
         """
         if self.in_rand:
-            positions = random_lattice()
+            positions = self.random_lattice()
         else:
-            positions = regular_lattice()
+            positions = self.regular_lattice()
 
-        mask = utils.in_circles_mask(positions, self.centers(), self.rad)
+        mask = utils.in_circles_mask(
+            np.mod(positions, self.super_cell_size()),
+            self.centers(),
+            self.rad)
         return positions[mask]
 
-    def out_circles(self, positions):
+    def out_circles(self):
         """
         Return the portion of the sample outside the circles.
         """
         if self.out_rand:
-            positions = random_lattice()
+            positions = self.random_lattice()
         else:
-            positions = regular_lattice()
+            positions = self.regular_lattice()
 
-        mask = utils.out_circles_mask(positions, self.centers(), self.rad)
+        mask = utils.out_circles_mask(
+            np.mod(positions, self.super_cell_size()),
+            self.centers(),
+            self.rad)
         return positions[mask]
 
+    def positions(self):
+        return np.vstack(in_circles(), out_circles())
+
+
+if __name__ == '__main__':
+    s = Sample(5.0, 5.0, 4.0)
+    in_circles = s.in_circles()
+    out_circles = s.out_circles()
+
+    plt.plot(in_circles[:, 0], in_circles[:, 1], '.r')
+    plt.plot(out_circles[:, 0], out_circles[:, 1], '.g')
+
+    plt.show()
--- pcsample/utils.py
+++ pcsample/utils.py
@@ -1,13 +1,15 @@
 import numpy as np
 
+
 def in_circle_mask(positions, center, rad):
     """
     Returns a numpy mask of the positions laying inside the circle
     centered at `center` with radius `rad`.
     """
     # Accomplished trough Array broadcasting
-    norms = np.sqrt(np.sum((positions - center) ** 2, axis = 1))
+    norms = np.sqrt(np.sum((positions - center) ** 2, axis=1))
     return norms <= rad
+
 
 def out_circle_mask(positions, center, rad):
     """
@@ -16,6 +18,7 @@ def out_circle_mask(positions, center, rad):
     """
     # Accomplished using the DRY principle
     return np.logical_not(in_circle_mask(positions, center, rad))
+
 
 def in_circles_mask(positions, centers, rad):
     """
@@ -26,9 +29,10 @@ def in_circles_mask(positions, centers, rad):
     mask.fill(False)
 
     for center in centers:
-        mask = np.logical_or(mask, in_circles_mask(positions, center, rad))
+        mask = np.logical_or(mask, in_circle_mask(positions, center, rad))
 
     return mask
+
 
 def out_circles_mask(positions, centers, rad):
     """
